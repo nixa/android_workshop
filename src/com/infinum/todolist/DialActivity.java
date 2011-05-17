9c0007afdabd22b61d533aa7ca9260e24ebb91ef
Nikola Kapraljevic nixa <nikola.kapraljevic@gmail.com>
2011-05-17 20:01:57 +0200

    Dial activity

---
+++ src/com/infinum/todolist/DialActivity.java
@@ -0,0 +1,27 @@
+package com.infinum.todolist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class DialActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dial);
+		
+		Button dialButton = (Button) findViewById(R.id.button1);
+		dialButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:0959115614"));
+				startActivity(intent);
+			}
+		});
+	}
+}
